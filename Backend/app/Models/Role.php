2094367d6bd5eae01015d898d4ff49b46f9255b7
Manisha <manisha04@graduate.utm.my>
2025-11-05 10:52:47 +0800

    Create user module database(role&user table

---
+++ Backend/app/Models/Role.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+
+class Role extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $primaryKey = 'role_id';
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected $fillable = ['role_name'];
+
+    public function users()
+    {
+        return $this->hasMany(User::class, 'role_id');
+    }
+}
+
