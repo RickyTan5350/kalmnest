e7c4942048f2697e18141ab3eb29e2175db86350
Manisha <manisha04@graduate.utm.my>
2025-11-05 10:55:04 +0800

    update models

--- Backend/app/Models/Role.php
+++ Backend/app/Models/Role.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
+
 class Role extends Model
 {
     use HasFactory, HasUuids;
