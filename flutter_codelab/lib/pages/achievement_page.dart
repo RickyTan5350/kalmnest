5bbf48017775de6ccce8c1de5354f43b5d282a0e
lokepeien <lokepeien9@gmail.com>
2025-11-09 23:52:58 +0800

    achivement header

--- flutter_codelab/lib/pages/achievement_page.dart
+++ flutter_codelab/lib/pages/achievement_page.dart
@@ -5,8 +5,30 @@ class AchievementPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Achievement'),
+    return Padding(
+      padding: const EdgeInsets.all(16.0), // Padding around the card
+      child: Card(
+        elevation: 2.0,
+        child: SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0), // Padding inside the card
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Achievements',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+                const SizedBox(height: 16),
+                const Text('---Achievements---'),
+                // Add more widgets to this Column
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
