ca4f236bf8b6fb3bc8f8df916636ef77754fb1ac
lokepeien <lokepeien9@gmail.com>
2025-11-09 23:51:16 +0800

    6 modules

---
+++ flutter_codelab/lib/pages/note_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class NotePage extends StatelessWidget {
+  const NotePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Note'),
+    );
+  }
+}
