920595ce37787b6d50bc4311bee84ece3746d957
lokepeien <lokepeien9@gmail.com>
2025-11-10 08:59:32 +0800

    note

--- flutter_codelab/lib/pages/note_page.dart
+++ flutter_codelab/lib/pages/note_page.dart
@@ -5,8 +5,30 @@ class NotePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Note'),
+    return Padding(
+      padding: const EdgeInsets.all(16.0), // Padding around the card
+      child: Card(
+        elevation: 2.0,
+        child: SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0), // Padding inside the card
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Note',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+                const SizedBox(height: 16),
+                const Text('---Note---'),
+                // Add more widgets to this Column
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
