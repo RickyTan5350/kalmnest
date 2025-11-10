a0db2d44bd8ac0471b44b466fc87fbc427df7250
lokepeien <lokepeien9@gmail.com>
2025-11-10 09:28:56 +0800

    user page

--- flutter_codelab/lib/pages/user_page.dart
+++ flutter_codelab/lib/pages/user_page.dart
@@ -5,8 +5,30 @@ class UserPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('User'),
+    return Padding(
+      padding: const EdgeInsets.all(16.0), // Padding around the card
+      child: Card(
+        elevation: 2.0,
+        child: SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0), // Padding inside the card
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Users',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+                const SizedBox(height: 16),
+                const Text('---Users---'),
+                // Add more widgets to this Column
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
