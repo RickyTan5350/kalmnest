89a824c240eedc5cf350f86ba9887be9b3516b94
siautm <148403382+siautm@users.noreply.github.com>
2025-11-21 00:17:30 +0800

    Update class_api.dart

--- flutter_codelab/lib/api/class_api.dart
+++ flutter_codelab/lib/api/class_api.dart
@@ -1,10 +1,11 @@
 // lib/api/class_api.dart
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import '../models/class_data.dart';
 
 class ClassApi {
   // Replace with your PC's local IP
-  static const String base = 'http://10.216.98.9:8000/api';
+  static const String base = 'http://127.0.0.1:8000/api';
 
   static Future<Map<String, dynamic>> createClass({
     required String className,
@@ -51,5 +52,40 @@ class ClassApi {
       print('Network error: $e');
       return {'success': false, 'message': 'Network error: $e'};
     }
+  }
+
+  static Future<Map<String, dynamic>> fetchClasses(int page) async {
+    final uri = Uri.parse(
+      "$base/classes?page=$page&per_page=5",
+    );
+
+    final res = await http.get(uri, headers: {'Accept': 'application/json'});
+
+    if (res.statusCode == 200) {
+      return jsonDecode(res.body);
+    } else {
+      throw Exception("Failed to load classes");
+    }
+  }
+
+  static Future<bool> updateClass(String id, Map<String, dynamic> data) async {
+    final response = await http.put(
+      Uri.parse("$base/classes/$id"),
+      headers: {"Content-Type": "application/json"},
+      body: json.encode(data),
+    );
+
+    return response.statusCode == 200;
+  }
+
+  static Future<bool> deleteClass(String id) async {
+    final response = await http.delete(
+      Uri.parse("$base/classes/$id"),
+    );
+
+    print("DELETE STATUS: ${response.statusCode}");
+    print("DELETE BODY: ${response.body}");
+
+    return response.statusCode == 200;
   }
 }
