b31c6891ff533628f9078c592f91133f7fa45e7e
siautm <148403382+siautm@users.noreply.github.com>
2025-11-21 00:12:44 +0800

    Update web.php

--- laravel_backend/routes/web.php
+++ laravel_backend/routes/web.php
@@ -1,74 +1,11 @@
-<!-- routes/web.php -->
 <?php
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\ClassController;
+Route::get('/', function () {
+    return 'Laravel backend is running!';
+});
 
-Route::get('/', [ClassController::class, 'index'])->name('classes.index');
-Route::get('/classes/create', [ClassController::class, 'create'])->name('classes.create');
-Route::post('/classes', [ClassController::class, 'store'])->name('classes.store');
-
-// class Task
-// {
-//   public function __construct(
-//     public int $id,
-//     public string $title,
-//     public string $description,
-//     public ?string $long_description,
-//     public bool $completed,
-//     public string $created_at,
-//     public string $updated_at
-//   ) {
-//   }
-// }
-
-// $tasks = [
-//   new Task(
-//     1,
-//     'Buy groceries',
-//     'Task 1 description',
-//     'Task 1 long description',
-//     false,
-//     '2023-03-01 12:00:00',
-//     '2023-03-01 12:00:00'
-//   ),
-//   new Task(
-//     2,
-//     'Sell old stuff',
-//     'Task 2 description',
-//     null,
-//     false,
-//     '2023-03-02 12:00:00',
-//     '2023-03-02 12:00:00'
-//   ),
-//   new Task(
-//     3,
-//     'Learn programming',
-//     'Task 3 description',
-//     'Task 3 long description',
-//     true,
-//     '2023-03-03 12:00:00',
-//     '2023-03-03 12:00:00'
-//   ),
-//   new Task(
-//     4,
-//     'Take dogs for a walk',
-//     'Task 4 description',
-//     null,
-//     false,
-//     '2023-03-04 12:00:00',
-//     '2023-03-04 12:00:00'
-//   ),
-// ];
-
-// Route::get("/",function() use ($tasks) {
-//     return view('index',[
-// 'task'=>$tasks
-//     ]);
-// })->name("tasks.index");
-
-// Route::get("/{id}",function($id) use ($tasks) {
-//     return 'Your visited task' . $id;
-// })->name("tasks.show");
-
+Route::prefix('api')->group(function () {
+    require base_path('routes/api.php');
+});
