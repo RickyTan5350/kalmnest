8d55392186552b9f5b37a052540f671543bc36fb
siautm <148403382+siautm@users.noreply.github.com>
2025-12-19 11:22:55 +0800

    Update API URLs for testing environment

--- flutter_codelab/lib/constants/api_constants.dart
+++ flutter_codelab/lib/constants/api_constants.dart
@@ -29,8 +29,8 @@ class ApiConstants {
     }
 
     if (kIsWeb) {
-      //   return 'https://kalmnest.test/api';
-      return 'https://backend_services.test/api';
+         return 'https://kalmnest.test/api';
+      
     }
 
     // If we are debugging on a physical device (Android OR iOS)
@@ -48,8 +48,8 @@ class ApiConstants {
       return 'https://10.0.2.2/api';
     } else {
       // Fallback for iOS Simulator
-      //   return 'https://kalmnest.test/api';
-      return 'https://backend_services.test/api';
+        return 'https://kalmnest.test/api';
+     
     }
   }
 
@@ -62,8 +62,8 @@ class ApiConstants {
     }
 
     if (kIsWeb) {
-      //  return 'https://kalmnest.test';
-      return 'https://backend_services.test';
+       return 'https://kalmnest.test';
+    
     }
 
     // Physical Device Support
@@ -77,8 +77,8 @@ class ApiConstants {
     if (Platform.isAndroid) {
       return 'https://10.0.2.2';
     } else {
-      //  return 'https://kalmnest.test';
-      return 'https://backend_services.test';
+       return 'https://kalmnest.test';
+    
     }
   }
 }
